305c30959089e3c8fe623836db8937ef2e4b2936
Pete Steward <p.steward@cgiar.org>
2023-05-12 13:46:19 +0300

    chirts dl added

--- R/1_download_datasets.R
+++ R/1_download_datasets.R
@@ -3,15 +3,22 @@ if(!chirps_dl){
   devtools::source_url("https://raw.githubusercontent.com/AdaptationAtlas/seasonality/main/R/functions/download_chirps.R")
 
   options(timeout = 150000)
-  download_chirps(start_year=1981,end_year = 2022,end_day=365,save_dir = chirps_dir,quiet=T)
+  download_chirps(start_year=1980,end_year = 2022,end_day=365,save_dir = chirps_dir,quiet=T)
 
 }
 
 # Download hobbins
-if(!hobbinss_dl){
+if(!hobbins_dl){
   devtools::source_url("https://raw.githubusercontent.com/AdaptationAtlas/seasonality/main/R/functions/download_hobbins.R")
+  devtools::source_url("https://raw.githubusercontent.com/AdaptationAtlas/seasonality/main/R/functions/download_and_unzip.R")
 
   options(timeout = 150000)
-  download_hobbins(start_year=1981,end_year = 2022,end_day=365,save_dir = chirps_dir,quiet=T)
+  download_hobbins(start_year=1980,end_year = 2022,end_dekad=1,save_dir = hobbins_dir,quiet=T)
 
+}
+
+# Download chirts
+if(!chirts_dl){
+  devtools::source_url("https://raw.githubusercontent.com/AdaptationAtlas/seasonality/main/R/functions/download_chirts.R")
+  download_chirts(start_year=1983,end_year=2016,save_dir=chirps_dir,quiet=F)
 }
