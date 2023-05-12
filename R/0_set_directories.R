235ea57420058cfc01158ea0e0d9a18cd935e670
Pete Steward <p.steward@cgiar.org>
2023-05-12 11:47:09 +0300

    reorganizing scripts

--- R/0_set_directories.R
+++ R/0_set_directories.R
@@ -1,6 +1,30 @@
+if(!require(pacman)){
+  install.packages("pacman")
+  library(pacman)
+}
+
+packages<-c()
+
+pacman::p_load(packages)
+
+
+if(F){
+# CGlabs
 data_dir<-"/home/jovyan/common_data"
 output_dir<-"/home/jovyan/common_data/atlas_sos"
 
-chirps_dir<-paste0(DataDir,"/chirps/raw")
-hobbins_dir<-paste0(DataDir,"/hobbins_ref_et/raw")
-chirts_dir<-paste0(DataDir,"/chirts")
+}else{
+
+# PS local machine
+data_dir<-"D:/datasets"
+output_dir<-"C:/datasets/seasonality"
+}
+
+chirps_dir<-paste0(data_dir,"/chirps/raw")
+hobbins_dir<-paste0(data_dir,"/hobbins_ref_et/raw")
+chirts_dir<-paste0(data_dir,"/chirts/raw")
+
+
+if(!dir.exists(output_dir)){
+  dir.create(output_dir,recursive = T)
+}
